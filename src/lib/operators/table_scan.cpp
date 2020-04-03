He3lixxx/hyrise

commit dcba300cefe324857b015e378c21deab8d2123f2

Use SingleChunkPosList in some places

src/lib/operators/table_scan.cpp
@@ -118,7 +118,7 @@ std::shared_ptr<const Table> TableScan::_on_execute() {
        *     (i.e. they share their position list).
        */
       if (in_table->type() == TableType::References) {
-        auto filtered_pos_lists = std::map<std::shared_ptr<const AbstractPosList>, std::shared_ptr<RowIDPosList>>{};
+        auto filtered_pos_lists = std::map<std::shared_ptr<const AbstractPosList>, std::shared_ptr<AbstractPosList>>{};
 
         for (ColumnID column_id{0u}; column_id < in_table->column_count(); ++column_id) {
           auto segment_in = chunk_in->get_segment(column_id);
@@ -134,16 +134,19 @@ std::shared_ptr<const Table> TableScan::_on_execute() {
           auto& filtered_pos_list = filtered_pos_lists[pos_list_in];
 
           if (!filtered_pos_list) {
-            filtered_pos_list = std::make_shared<RowIDPosList>(matches_out->size());
             if (pos_list_in->references_single_chunk()) {
-              filtered_pos_list->guarantee_single_chunk();
-            }
-
-            size_t offset = 0;
-            for (const auto& match : *matches_out) {
-              const auto row_id = (*pos_list_in)[match.chunk_offset];
-              (*filtered_pos_list)[offset] = row_id;
-              ++offset;
+              size_t index = 0;
+              std::vector<ChunkOffset> offsets(matches_out->size());
+              for (const auto& match : *matches_out) {
+                offsets[index++] = (*pos_list_in)[match.chunk_offset].chunk_offset;
+              }
+              filtered_pos_list = std::make_shared<SingleChunkPosList>(pos_list_in->common_chunk_id(), std::move(offsets));
+            } else {
+              filtered_pos_list = std::make_shared<RowIDPosList>(matches_out->size());
+              size_t index = 0;
+              for (const auto& match : *matches_out) {
+                (*filtered_pos_list)[index++] = (*pos_list_in)[match.chunk_offset];
+              }
             }
           }
 

src/lib/operators/limit.cpp
@@ -10,6 +10,7 @@
 #include "expression/expression_utils.hpp"
 #include "storage/reference_segment.hpp"
 #include "storage/table.hpp"
+#include "storage/pos_lists/entire_chunk_pos_list.hpp"
 
 namespace opossum {
 
@@ -73,22 +74,20 @@ std::shared_ptr<const Table> Limit::_on_execute() {
 
     for (ColumnID column_id{0}; column_id < input_table->column_count(); column_id++) {
       const auto input_base_segment = input_chunk->get_segment(column_id);
-      auto output_pos_list = std::make_shared<RowIDPosList>(output_chunk_row_count);
       std::shared_ptr<const Table> referenced_table;
       ColumnID output_column_id = column_id;
 
+      std::shared_ptr<AbstractPosList> output_pos_list;
       if (auto input_ref_segment = std::dynamic_pointer_cast<const ReferenceSegment>(input_base_segment)) {
         output_column_id = input_ref_segment->referenced_column_id();
         referenced_table = input_ref_segment->referenced_table();
         // TODO(all): optimize using whole chunk whenever possible
         auto begin = input_ref_segment->pos_list()->begin();
-        std::copy(begin, begin + output_chunk_row_count, output_pos_list->begin());
+        output_pos_list = std::make_shared<RowIDPosList>(begin, begin + output_chunk_row_count);
       } else {
         referenced_table = input_table;
-        for (ChunkOffset chunk_offset = 0; chunk_offset < static_cast<ChunkOffset>(output_chunk_row_count);
-             chunk_offset++) {
-          (*output_pos_list)[chunk_offset] = RowID{chunk_id, chunk_offset};
-        }
+
+        output_pos_list = std::make_shared<EntireChunkPosList>(chunk_id, output_chunk_row_count);
       }
 
       output_segments.push_back(

src/lib/operators/validate.cpp
@@ -10,6 +10,7 @@
 #include "operators/delete.hpp"
 #include "scheduler/job_task.hpp"
 #include "storage/pos_lists/entire_chunk_pos_list.hpp"
+#include "storage/pos_lists/single_chunk_pos_list.hpp"
 #include "storage/reference_segment.hpp"
 #include "utils/assert.hpp"
 
@@ -215,16 +216,15 @@ void Validate::_validate_chunks(const std::shared_ptr<const Table>& in_table, co
         pos_list_out = std::make_shared<EntireChunkPosList>(chunk_id, chunk_in->size());
       } else {
         const auto mvcc_data = chunk_in->mvcc_data();
-        RowIDPosList temp_pos_list;
-        temp_pos_list.guarantee_single_chunk();
-        // Generate pos_list_out.
+        std::vector<ChunkOffset> chunk_offsets;
+
         auto chunk_size = chunk_in->size();  // The compiler fails to optimize this in the for clause :(
         for (auto chunk_offset = 0u; chunk_offset < chunk_size; chunk_offset++) {
           if (opossum::is_row_visible(our_tid, snapshot_commit_id, chunk_offset, *mvcc_data)) {
-            temp_pos_list.emplace_back(RowID{chunk_id, chunk_offset});
+            chunk_offsets.emplace_back(chunk_offset);
           }
         }
-        pos_list_out = std::make_shared<const RowIDPosList>(std::move(temp_pos_list));
+        pos_list_out = std::make_shared<SingleChunkPosList>(chunk_id, std::move(chunk_offsets));
       }
 
       // Create actual ReferenceSegment objects.

src/lib/storage/split_pos_list_by_chunk_id.cpp
@@ -16,9 +16,8 @@ PosListsByChunkID split_pos_list_by_chunk_id(const std::shared_ptr<const Abstrac
   for (auto chunk_id = ChunkID{0}; chunk_id < number_of_chunks; ++chunk_id) {
     DebugAssert(chunk_id < number_of_chunks, "Inconsistent number_of_chunks passed");
     auto& mapping = pos_lists_by_chunk_id[chunk_id];
-    mapping.row_ids = std::make_shared<RowIDPosList>();
-    mapping.row_ids->guarantee_single_chunk();
-    mapping.row_ids->reserve(input_pos_list->size() / number_of_chunks);
+    mapping.row_ids = std::make_shared<SingleChunkPosList>(chunk_id);
+    mapping.row_ids->get_offsets().reserve(input_pos_list->size() / number_of_chunks);
     mapping.original_positions.reserve(input_pos_list->size() / number_of_chunks);
   }
 
@@ -32,7 +31,7 @@ PosListsByChunkID split_pos_list_by_chunk_id(const std::shared_ptr<const Abstrac
 
     auto& mapping = pos_lists_by_chunk_id[row_id.chunk_id];
 
-    mapping.row_ids->emplace_back(row_id);
+    mapping.row_ids->get_offsets().emplace_back(row_id.chunk_offset);
     mapping.original_positions.emplace_back(original_position++);
   }
 

src/lib/storage/split_pos_list_by_chunk_id.hpp
@@ -3,7 +3,7 @@
 #include <unordered_map>
 #include <vector>
 
-#include "storage/pos_lists/row_id_pos_list.hpp"
+#include "storage/pos_lists/single_chunk_pos_list.hpp"
 #include "types.hpp"
 #include "uninitialized_vector.hpp"
 
@@ -13,7 +13,7 @@ namespace opossum {
 // of which references only a single chunk. For each entry in that SubPosList, we need to keep its position in the
 // original PosList so that we can reassemble that PosList if needed.
 struct SubPosList {
-  std::shared_ptr<RowIDPosList> row_ids;
+  std::shared_ptr<SingleChunkPosList> row_ids;
   std::vector<ChunkOffset> original_positions;
 };
 

commit 5bd86d3688754d4d568f3b60ac0967eeb3b2a1c6

Fix broken tables scan (broken since dcba300ce)

src/lib/operators/table_scan.cpp
@@ -147,16 +147,18 @@ std::shared_ptr<const Table> TableScan::_on_execute() {
               });
               filtered_pos_list = std::make_shared<SingleChunkPosList>(pos_list_in->common_chunk_id(), std::move(offsets));
             } else {
-              filtered_pos_list = std::make_shared<RowIDPosList>(matches_out->size());
+              auto temp_pos_list = std::make_shared<RowIDPosList>(matches_out->size());
+
               size_t index = 0;
               resolve_pos_list_type(matches_out, [&](auto& typed_matches_out) {
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wrange-loop-analysis"
                 for (const auto match : *typed_matches_out) {
-                  (*filtered_pos_list)[index++] = (*pos_list_in)[match.chunk_offset];
+                  (*temp_pos_list)[index++] = (*pos_list_in)[match.chunk_offset];
                 }
 #pragma clang diagnostic pop
               });
+              filtered_pos_list = temp_pos_list;
             }
           }
 